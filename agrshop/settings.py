b3b6b856732e48592ae1b9782e9edfc49ed390bb
Your Name <your.email@example.com>
2025-05-07 21:25:56 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- agrshop/settings.py
+++ agrshop/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['agr-shop.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
